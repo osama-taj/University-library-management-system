e9db7fe9facf303ca0515ea56f1e0a46b5b5a3c7
osama-taj <osamahtaj2020@gmail.com>
2023-02-10 21:52:23 +0300

    add DB file

--- ProjectLibliry/My Project/Application.Designer.vb
+++ ProjectLibliry/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ProjectLibliry.main
+            Me.MainForm = Global.ProjectLibliry.Form1
         End Sub
     End Class
 End Namespace
